6792784f7ccb2eaa4c162638a1510a3c4fc00e9e
Tran Duc <v.ductran@vinbrain.net>
2020-05-04 15:54:54 +0700

    Add more price type

--- _research/fxqu4nt/fxqu4nt/market/constant.py
+++ _research/fxqu4nt/fxqu4nt/market/constant.py
@@ -4,10 +4,14 @@ import enum
 class PriceType(enum.Enum):
     Ask = 1
     Bid = 2
-    Open = 3
-    High = 4
-    Low = 5
-    Close = 6
+    BidOpen = 3
+    BidHigh = 4
+    BidLow = 5
+    BidClose = 6
+    AskOpen = 3
+    AskHigh = 4
+    AskLow = 5
+    AskClose = 6
 
     @staticmethod
     def from_string(s):
@@ -15,12 +19,20 @@ class PriceType(enum.Enum):
             return PriceType.Ask
         elif s == PriceType.Bid.name:
             return PriceType.Bid
-        elif s == PriceType.Open.name:
-            return PriceType.Open
-        elif s == PriceType.High.name:
-            return PriceType.High
-        elif s == PriceType.Low.name:
-            return PriceType.Low
-        elif s == PriceType.Close.name:
-            return PriceType.Close
+        elif s == PriceType.BidOpen.name:
+            return PriceType.BidOpen
+        elif s == PriceType.BidHigh.name:
+            return PriceType.BidHigh
+        elif s == PriceType.BidLow.name:
+            return PriceType.BidLow
+        elif s == PriceType.BidClose.name:
+            return PriceType.BidClose
+        elif s == PriceType.AskOpen.name:
+            return PriceType.AskOpen
+        elif s == PriceType.AskHigh.name:
+            return PriceType.AskHigh
+        elif s == PriceType.AskLow.name:
+            return PriceType.AskLow
+        elif s == PriceType.AskClose.name:
+            return PriceType.AskClose
 
